f940b591fe4af8d656c9cf4c107c8d599e0e2c3d
Amit Talreja <atalreja@berkeley.edu>
2018-04-23 12:05:52 -0700

    adding generator for loading training data

--- examples/conv_edges.py
+++ examples/conv_edges.py
@@ -12,7 +12,7 @@ from keras.utils import plot_model
 from perception import GrayscaleImage, BinaryImage, DepthImage
 from visualization import Visualizer2D as vis2d
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 def generate_training_data(filepath):
     sigma = 0.3
@@ -64,13 +64,15 @@ def depth_image_train_data(depth_image_path, edge_mask_path,  n, dim=256):
     y = y[:,:,:,np.newaxis] / 255.0
     return x, y
 
-def depth_image_normals_train_data(x_path, y_path, n, dim=256): 
+def depth_image_normals_train_data(x_path, y_path, n, dim=256, start=0): 
     x = np.zeros((n, dim, dim, 4))
     y = np.zeros((n, dim, dim))
 
     for i in range(n):
-        di = np.load(x_path + str(i) + '.npy')
-        edge_mask = np.load(y_path + str(i) + '.npy')
+        if i % 1000 == 0:
+            print (i)
+        di = np.load(x_path + str(start + i) + '.npy')
+        edge_mask = np.load(y_path + str(start + i) + '.npy')
         #rescale the depth image 
         min_depth = np.min(di[di > 0])
         max_depth = np.max(di[di > 0])
@@ -81,39 +83,71 @@ def depth_image_normals_train_data(x_path, y_path, n, dim=256):
     y = y[:, :, :, np.newaxis] / 255.0
     return x,y
 
+def train_generator(x_path, y_path, dim=256):
+    file_idx = 0
+    batch_size = 32
+    while True:
+        try:
+            if file_idx >= 50000:
+                file_idx = 0
+            x = np.zeros((batch_size, dim, dim, 4))
+            y = np.zeros((batch_size, dim, dim))
+
+            for i in range(batch_size):
+                di = np.load(x_path + str(file_idx + i) + '.npy')
+                edge_mask = np.load(y_path + str(file_idx + i) + '.npy')
+                #rescale the depth image 
+                min_depth = np.min(di[di > 0])
+                max_depth = np.max(di[di > 0])
+                di = (di - min_depth) / (max_depth - min_depth)
+                x[i, :, :, :] = di
+                y[i, :, :] = edge_mask
+        
+            file_idx += 32
+            y = y[:, :, :, np.newaxis] / 255.0
+            yield x, y
+        except ValueError:
+            file_idx += 32
+
+tg = train_generator('./data/thingiverse_depth/', './data/thingiverse_edge/')
+
+
+
 # get training data and test data
 
 # train_data, train_labels = generate_training_data('./data/rgb_images/')
 #data, labels = depth_image_train_data('./data/depth_images/', './data/edge_masks/', n=2000)
-#data, labels = depth_image_normals_train_data('./data/barclamp_depth_normal/', './data/barclamp_edge_normal/', n=1000)
+#data, labels = depth_image_normals_train_data('./data/thingiverse_depth/', './data/thingiverse_edge/', n=10000)
 #percent_train = 0.9
 #split_idx = int(percent_train * data.shape[0])
 #train_data, train_labels = data[:split_idx, :, :, :], labels[:split_idx, :, :, :]
 #test_data, test_labels = data[split_idx:, : ,: , :], labels[split_idx:, :, :, :]
 #test_data, test_labels = depth_image_normals_train_data('./data/demon_depth_normal/', './data/demon_edge_normal/', n=1000)
+test_data, test_labels = depth_image_normals_train_data('./data/thingiverse_depth/', './data/thingiverse_edge/', n=1000, start=60000)
+
 
 ## have training data from two folders:
-b_data, b_labels = depth_image_normals_train_data('./data/barclamp_depth_normal/', './data/barclamp_edge_normal/', n=1000)
-d_data, d_labels = depth_image_normals_train_data('./data/demon_depth_normal/', './data/demon_edge_normal/', n=1000)
+#b_data, b_labels = depth_image_normals_train_data('./data/barclamp_depth_normal/', './data/barclamp_edge_normal/', n=1000)
+#d_data, d_labels = depth_image_normals_train_data('./data/demon_depth_normal/', './data/demon_edge_normal/', n=1000)
 
-percent_train = 0.5
-split_idx = int(percent_train * b_data.shape[0])
+#percent_train = 0.5
+#split_idx = int(percent_train * b_data.shape[0])
 
-b_train, b_train_labels = b_data[:split_idx, :, :, :], b_labels[:split_idx, :, :, :]
-b_test, b_test_labels = b_data[split_idx:, :, :, :], b_labels[split_idx:, :, :, :]
+#b_train, b_train_labels = b_data[:split_idx, :, :, :], b_labels[:split_idx, :, :, :]
+#b_test, b_test_labels = b_data[split_idx:, :, :, :], b_labels[split_idx:, :, :, :]
 
-d_train, d_train_labels = d_data[:split_idx, :, :, :], d_labels[:split_idx, :, :, :]
-d_test, d_test_labels = d_data[split_idx:, :, :, :], d_labels[split_idx:, :, :, :]
+#d_train, d_train_labels = d_data[:split_idx, :, :, :], d_labels[:split_idx, :, :, :]
+#d_test, d_test_labels = d_data[split_idx:, :, :, :], d_labels[split_idx:, :, :, :]
 
-train_data, train_labels = np.concatenate((b_train, d_train)), np.concatenate((b_train_labels, d_train_labels))
-test_data, test_labels = np.concatenate((b_test, d_test)), np.concatenate((b_test_labels, d_test_labels))
+#train_data, train_labels = np.concatenate((b_train, d_train)), np.concatenate((b_train_labels, d_train_labels))
+#test_data, test_labels = np.concatenate((b_test, d_test)), np.concatenate((b_test_labels, d_test_labels))
 
 # set options for conv net
 window_size = (7,7)
 input_size = (256,256, 4)
 # input_size = (256, 256, 3)
 # input_size = (256, 256, 1)
-num_epochs = 50
+num_epochs = 10
 
 model = Sequential()
 
@@ -128,13 +162,13 @@ model.compile(loss=binary_crossentropy,
               optimizer=Adam(lr=0.01),
               metrics=['accuracy'])
 
-model.fit(train_data, train_labels, verbose=1, epochs=num_epochs)
-
+model.fit_generator(tg,  verbose=1, epochs=num_epochs, steps_per_epoch=1562)
+model.save("edge_cnn_10_epochs.h5")
 # test_data, test_labels = generate_training_data('./data/test/')
 
-predicted_edge_mask = model.predict(test_data)
+predicted_edge_masks = model.predict(test_data)
 
-predicted_edge_mask = np.array(predicted_edge_mask[3] * 255, dtype=np.uint8)
+predicted_edge_mask = np.array(predicted_edge_masks[3] * 255, dtype=np.uint8)
 gi = BinaryImage(predicted_edge_mask)
 actual_edge_mask = np.array(test_labels[3] * 255, dtype=np.uint8)
 bi = BinaryImage(actual_edge_mask)
@@ -146,8 +180,28 @@ vis2d.imshow(bi)
 vis2d.show()
 
 
+predicted_edge_mask = np.array(predicted_edge_masks[203] * 255, dtype=np.uint8)
+gi = BinaryImage(predicted_edge_mask)
+actual_edge_mask = np.array(test_labels[203] * 255, dtype=np.uint8)
+bi = BinaryImage(actual_edge_mask)
+vis2d.figure()
+vis2d.subplot(121)
+vis2d.imshow(gi)
+vis2d.subplot(122)
+vis2d.imshow(bi)
+vis2d.show()
 
 
+predicted_edge_mask = np.array(predicted_edge_masks[503] * 255, dtype=np.uint8)
+gi = BinaryImage(predicted_edge_mask)
+actual_edge_mask = np.array(test_labels[503] * 255, dtype=np.uint8)
+bi = BinaryImage(actual_edge_mask)
+vis2d.figure()
+vis2d.subplot(121)
+vis2d.imshow(gi)
+vis2d.subplot(122)
+vis2d.imshow(bi)
+vis2d.show()
 
 
 
